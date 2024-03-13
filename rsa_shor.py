6163ab319a6cf38ad25ea3bcb7eeaef42ae3a7f2
kazawai <bazinmaxime0@gmail.com>
2024-03-13 14:38:05 +0100

    Shor works in some instances.
    Oftentimes the algorithm will return (1, N) as the prime factors, which is obviously not correct. Will have to find the reason.

--- rsa_shor.py
+++ rsa_shor.py
@@ -7,11 +7,12 @@ import traceback
 # Time the execution of the algorithm
 from time import time
 
-from numpy import gcd, pi, zeros
+from numpy import ceil, gcd, log2, pi, zeros
 from numpy.random import randint, seed
 # For shor's algorithm in qiskit version 1.0.1
 from qiskit import (ClassicalRegister, QuantumCircuit, QuantumRegister,
                     transpile)
+from qiskit.circuit.quantumcircuit import Qubit, QubitSpecifier
 from qiskit_aer import AerSimulator
 
 
@@ -57,7 +58,8 @@ print(f"Decrypted text : {decrypted}")
 
 print("-" * 50)
 
-seed(1)
+# Set the seed for reproducible results
+# seed(1)
 
 N = public_key[1]
 
@@ -73,10 +75,12 @@ def qft(
     circuit: QuantumCircuit, qreg: QuantumRegister, n: int, with_swaps: bool = True
 ):
     """n-qubit QFT on q in circ."""
-    for j in range(n):
-        circuit.h(qreg[n - j - 1])
-        for k in range(n - j - 1):
-            circuit.cp(pi / float(2 ** (n - j - 1 - k)), qreg[k], qreg[n - j - 1])
+    for j in range(n - 1, 0, -1):
+        circuit.h(qreg[j])
+        for k in range(j - 1, -1, -1):
+            if (pi / float(2 ** (j - k))) > 0:
+                circuit.cp(pi / float(2 ** (j - k)), qreg[j], qreg[k])
+                k -= 1
         if with_swaps:
             circuit.barrier()
 
@@ -95,9 +99,11 @@ def qft_dagger(
             circuit.swap(qreg[j], qreg[n - j - 1])
 
     for j in range(n):
-        for k in range(j + 1, n):
-            circuit.cp(-pi / float(2 ** (k - j)), qreg[k], qreg[j])
         circuit.h(qreg[j])
+        if j != n - 1:
+            for k in range(j, -1, -1):
+                if (pi / float(2 ** ((j + 1) - k))) > 0:
+                    circuit.cp(-pi / float(2 ** ((j + 1) - k)), qreg[j + 1], qreg[k])
         if with_swaps:
             circuit.barrier()
     return circuit
@@ -117,11 +123,16 @@ def get_angles(a, N):
 
 def cc_phase_gate(
     circuit: QuantumCircuit,
-    control_qubits: list[QuantumRegister],
+    control_qubits: list[QubitSpecifier | Qubit | QuantumRegister],
     target_qubit: int,
     angle: float,
 ):
     """Create a doubly controlled phase gate"""
+    if len(control_qubits) != 2:
+        raise ValueError(
+            "The doubly controlled phase gate needs exactly 2 control qubits"
+        )
+
     circuit.cp(angle / 2, control_qubits[0], target_qubit)
     circuit.cx(control_qubits[1], control_qubits[0])
     circuit.cp(-angle / 2, control_qubits[0], target_qubit)
@@ -152,7 +163,7 @@ def controlled_phi_adder(
     qreg: QuantumRegister,
     a: int,
     N: int,
-    control_qubits: list[QuantumRegister] | QuantumRegister,
+    control_qubits: QuantumRegister | Qubit | list[Qubit],
     inverse: bool = False,
 ):
     """Controlled version of the phi_adder"""
@@ -161,40 +172,40 @@ def controlled_phi_adder(
         if len(angles) == 0:
             break
         if not inverse:
-            if isinstance(control_qubits, QuantumRegister) or len(control_qubits) == 1:
-                # Here, an error occurs when the control qubit and the target qubit are the same...
-                try:
-                    if control_qubits[0] != qreg[i]:
-                        circuit.cp(
-                            angles[i],
-                            (
-                                control_qubits
-                                if isinstance(control_qubits, QuantumRegister)
-                                else control_qubits[0]
-                            ),
-                            qreg[i],
-                        )
-                except Exception as e:
-                    print(traceback.format_exc())
-                    print(f"Control qubits: {control_qubits}")
-                    print(f"Target qubit: {qreg[i]}")
-                    print(f"Angle: {angles[i]}")
-                    raise e
+            if (
+                isinstance(control_qubits, QuantumRegister)
+                or isinstance(control_qubits, Qubit)
+                or len(control_qubits) == 1
+            ):
+                circuit.cp(
+                    angles[i],
+                    (
+                        control_qubits
+                        if isinstance(control_qubits, QuantumRegister)
+                        or isinstance(control_qubits, Qubit)
+                        else control_qubits[0]
+                    ),
+                    qreg[i],
+                )
 
             else:
                 cc_phase_gate(circuit, control_qubits, qreg[i], angles[i])
         else:
-            if isinstance(control_qubits, QuantumRegister) or len(control_qubits) == 1:
-                if control_qubits[0] != qreg[i]:
-                    circuit.cp(
-                        -angles[i],
-                        (
-                            control_qubits
-                            if isinstance(control_qubits, QuantumRegister)
-                            else control_qubits[0]
-                        ),
-                        qreg[i],
-                    )
+            if (
+                isinstance(control_qubits, QuantumRegister)
+                or isinstance(control_qubits, Qubit)
+                or len(control_qubits) == 1
+            ):
+                circuit.cp(
+                    -angles[i],
+                    (
+                        control_qubits
+                        if isinstance(control_qubits, QuantumRegister)
+                        or isinstance(control_qubits, Qubit)
+                        else control_qubits[0]
+                    ),
+                    qreg[i],
+                )
             else:
                 cc_phase_gate(circuit, control_qubits, qreg[i], -angles[i])
 
@@ -205,21 +216,21 @@ def controlled_phi_adder_mod_N(
     a: int,
     N: int,
     n: int,
-    control_qubits: list[QuantumRegister] | QuantumRegister,
-    ancilla_qubits: QuantumRegister,
+    control_qubits: QuantumRegister | Qubit | list[Qubit],
+    ancilla_qubit: Qubit,
 ):
     """Controlled version of the phi_adder"""
     controlled_phi_adder(circuit, qreg, a, n, control_qubits, False)
     phi_adder(circuit, qreg, N, n, True)
     qft_dagger(circuit, qreg, n, False)
-    circuit.cx(qreg[n - 1], ancilla_qubits[0])
+    circuit.cx(qreg[n - 1], ancilla_qubit)
     qft(circuit, qreg, n, False)
     controlled_phi_adder(circuit, qreg, N, n, control_qubits, False)
 
     controlled_phi_adder(circuit, qreg, a, n, control_qubits, True)
     qft_dagger(circuit, qreg, n, False)
     circuit.x(qreg[n - 1])
-    circuit.cx(qreg[n - 1], ancilla_qubits[0])
+    circuit.cx(qreg[n - 1], ancilla_qubit)
     circuit.x(qreg[n - 1])
     qft(circuit, qreg, n, False)
     controlled_phi_adder(circuit, qreg, a, n, control_qubits, False)
@@ -231,61 +242,66 @@ def controlled_phi_adder_mod_N_inv(
     a: int,
     N: int,
     n: int,
-    control_qubits: list[QuantumRegister] | QuantumRegister,
-    ancilla_qubits: QuantumRegister,
+    control_qubits: QuantumRegister | Qubit | list[Qubit],
+    ancilla_qubit: Qubit,
 ):
     """Controlled version of the inverse phi_adder"""
     controlled_phi_adder(circuit, qreg, a, n, control_qubits, True)
     qft_dagger(circuit, qreg, n, False)
     circuit.x(qreg[n - 1])
-    circuit.cx(qreg[n - 1], ancilla_qubits[0])
+    circuit.cx(qreg[n - 1], ancilla_qubit)
     circuit.x(qreg[n - 1])
     qft(circuit, qreg, n, False)
     controlled_phi_adder(circuit, qreg, a, n, control_qubits, False)
 
     controlled_phi_adder(circuit, qreg, N, n, control_qubits, False)
     qft_dagger(circuit, qreg, n, False)
-    circuit.cx(qreg[n - 1], ancilla_qubits[0])
+    circuit.cx(qreg[n - 1], ancilla_qubit)
     qft(circuit, qreg, n, False)
     phi_adder(circuit, qreg, N, n, False)
     controlled_phi_adder(circuit, qreg, a, n, control_qubits, True)
 
 
-def controller_mult_mod_N(
+def controlled_mult_mod_N(
     circuit: QuantumCircuit,
     qreg: QuantumRegister,
     a: int,
     N: int,
     n: int,
-    control_reg: list[QuantumRegister] | QuantumRegister,
+    control_reg: QuantumRegister | Qubit,
     ancilla_reg: QuantumRegister,
 ):
     """Controlled version of the multiplication by a mod N"""
-    qft(circuit, qreg, n, False)
+    qft(circuit, ancilla_reg, n + 1, False)
     for i in range(n):
         controlled_phi_adder_mod_N(
-            circuit, qreg, (2**i) * a % N, N, n, control_reg, ancilla_reg
+            circuit,
+            ancilla_reg,
+            (2**i) * a % N,
+            N,
+            n + 1,
+            [qreg[i], control_reg],
+            ancilla_reg[n + 1],
         )
-    qft_dagger(circuit, qreg, n, False)
+    qft_dagger(circuit, ancilla_reg, n + 1, False)
 
     for i in range(n):
-        if control_reg[0] != qreg[i]:
-            circuit.cswap(control_reg[0], qreg[i], ancilla_reg[i])
+        circuit.cswap(control_reg, qreg[i], ancilla_reg[i])
 
     a_inv = pow(a, -1, N)
-    qft(circuit, qreg, n + 1, False)
+    qft(circuit, ancilla_reg, n + 1, False)
 
     for i in range(n):
         controlled_phi_adder_mod_N_inv(
             circuit,
-            qreg,
+            ancilla_reg,
             pow(2, i) * a_inv % N,
             N,
             n + 1,
-            control_reg,
-            ancilla_reg,
+            [qreg[i], control_reg],
+            ancilla_reg[n + 1],
         )
-    qft_dagger(circuit, qreg, n, False)
+    qft_dagger(circuit, ancilla_reg, n + 1, False)
 
 
 def check_power(N):
@@ -317,15 +333,17 @@ def qpe_period_finding(N, a, n) -> float | str:
     qpe = QuantumCircuit(q_up, q_down, a_q, c)
     try:
         # Initialize qubits
-        qft(qpe, q_up, 2 * n, False)
+        qpe.h(q_up)
         qpe.x(q_down[0])
 
         # Apply controlled multiplication gates
         for i in range(n):
-            controller_mult_mod_N(qpe, q_up, a, N, n, q_down, a_q)
+            controlled_mult_mod_N(
+                qpe, q_down, int(pow(a, pow(2, i))), N, n, q_up[i], a_q
+            )
 
         # Apply inverse QFT
-        qft_dagger(qpe, q_up, n, False)
+        qft_dagger(qpe, q_up, 2 * n, True)
 
         # Measure the qubits
         qpe.measure(q_up, c)
@@ -341,8 +359,6 @@ def qpe_period_finding(N, a, n) -> float | str:
         # Extract the phase
         phase = max(counts, key=counts.get)
         phase = int(phase, 2) / 2**n
-
-        print(qpe.draw())
 
         return phase
     except Exception as e:
@@ -360,27 +376,35 @@ def shor(N):
 
     # Get a random coprime of N
     a = get_coprime(N)
+    print(f"The coprime of {N} is {a}")
 
     # Check if a is a non-trivial square root of 1
     if pow(a, N // 2, N) == (N - 1):
         return gcd(a + 1, N), gcd(a - 1, N)
 
     # Find the period
-    n = int(2 * (N**0.5)).bit_length()
+    n = int(ceil(log2(N)))
     phase = qpe_period_finding(N, a, n)
 
     print(f"The phase is {phase}")
 
     # Check if the period is even
     if isinstance(phase, float) and phase % 1 == 0:
-        return int((a ** (phase / 2) - 1) % N), int((a ** (phase / 2) + 1) % N)
-    else:
-        return "Failure"
+        if phase % 2 == 0:
+            x = int(pow(a, phase / 2))
+            if (x + 1) % N != 0:
+                # TODO : The factors oftentimes are 1 and N, which is not correct. Check the implementation
+                factors = gcd(x + 1, N), gcd(x - 1, N)
+                return factors
+
+    print("Failure, trying again")
+    return shor(N)
 
 
 if __name__ == "__main__":
     # Time the execution of the algorithm
     print("Algorithme quantique de Shor")
+    N = 15
     start = time()
     factors = shor(N)
     end = time()
